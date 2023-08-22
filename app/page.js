640f2867069d5f94e90b447694ff4039b495db6b
didar2016 <khandidar11@gmail.com>
2023-08-22 15:41:37 +0600

    2nd commit

--- app/page.js
+++ app/page.js
@@ -1,95 +1,260 @@
-import Image from 'next/image'
-import styles from './page.module.css'
+import Image from "next/image";
+import styles from "./page.module.css";
+import "bootstrap/dist/css/bootstrap.css";
+import "../node_modules/@fortawesome/fontawesome-svg-core/styles.css";
 
 export default function Home() {
   return (
-    <main className={styles.main}>
-      <div className={styles.description}>
-        <p>
-          Get started by editing&nbsp;
-          <code className={styles.code}>app/page.js</code>
-        </p>
-        <div>
-          <a
-            href="https://vercel.com?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            By{' '}
-            <Image
-              src="/vercel.svg"
-              alt="Vercel Logo"
-              className={styles.vercelLogo}
-              width={100}
-              height={24}
-              priority
-            />
-          </a>
+    <main>
+      <head>
+        <meta charset="utf-8" />
+        <meta
+          name="viewport"
+          content="width=device-width, initial-scale=1, shrink-to-fit=no"
+        />
+        <meta name="description" content="" />
+        <meta name="author" content="" />
+        <link
+          href="https://fonts.googleapis.com/css?family=Poppins:100,200,300,400,500,600,700,800,900&display=swap"
+          rel="stylesheet"
+        ></link>
+
+        <title>Hexashop Ecommerce HTML CSS Template</title>
+
+        <link
+          rel="stylesheet"
+          type="text/css"
+          href="assets/css/bootstrap.min.css"
+        ></link>
+
+        <link
+          rel="stylesheet"
+          type="text/css"
+          href="assets/css/font-awesome.css"
+        ></link>
+
+        <link rel="stylesheet" href="assets/css/templatemo-hexashop.css"></link>
+
+        <link rel="stylesheet" href="assets/css/owl-carousel.css"></link>
+
+        <link rel="stylesheet" href="assets/css/lightbox.css"></link>
+      </head>
+      {/* <!-- ***** Preloader Start ***** --> */}
+      {/* <div id="preloader">
+          <div className="jumper">
+            <div></div>
+            <div></div>
+            <div></div>
+          </div>
+        </div> */}
+      {/* <!-- ***** Preloader End ***** --> */}
+
+      {/* <!-- ***** Header Area Start ***** --> */}
+      <header className="header-area header-sticky">
+        <div className="container">
+          <div className="row">
+            <div className="col-12">
+              <nav className="main-nav">
+                {/* <!-- ***** Logo Start ***** --> */}
+                <a href="index.html" className="logo">
+                  <img
+                    src="assets/images/classy-logo.png"
+                    height={40}
+                    width={40}
+                  />
+                </a>
+                {/* <!-- ***** Logo End ***** --> */}
+                {/* <!-- ***** Menu Start ***** --> */}
+                <ul className="nav">
+                  <li className="scroll-to-section">
+                    <a href="#top" className="active">
+                      Home
+                    </a>
+                  </li>
+                  <li className="scroll-to-section">
+                    <a href="#men">Men's</a>
+                  </li>
+                  <li className="scroll-to-section">
+                    <a href="#women">Women's</a>
+                  </li>
+                  <li className="scroll-to-section">
+                    <a href="#kids">Kid's</a>
+                  </li>
+                  <li className="submenu">
+                    <a href="javascript:;">Pages</a>
+                    <ul>
+                      <li>
+                        <a href="about.html">About Us</a>
+                      </li>
+                      <li>
+                        <a href="products.html">Products</a>
+                      </li>
+                      <li>
+                        <a href="single-product.html">Single Product</a>
+                      </li>
+                      <li>
+                        <a href="contact.html">Contact Us</a>
+                      </li>
+                    </ul>
+                  </li>
+                  <li className="submenu">
+                    <a href="javascript:;">Features</a>
+                    <ul>
+                      <li>
+                        <a href="#">Features Page 1</a>
+                      </li>
+                      <li>
+                        <a href="#">Features Page 2</a>
+                      </li>
+                      <li>
+                        <a href="#">Features Page 3</a>
+                      </li>
+                      <li>
+                        <a
+                          rel="nofollow"
+                          href="https://templatemo.com/page/4"
+                          target="_blank"
+                        >
+                          Template Page 4
+                        </a>
+                      </li>
+                    </ul>
+                  </li>
+                  <li className="scroll-to-section">
+                    <a href="#explore">Explore</a>
+                  </li>
+                </ul>
+                <a className="menu-trigger">
+                  <span>Menu</span>
+                </a>
+                {/* <!-- ***** Menu End ***** --> */}
+              </nav>
+            </div>
+          </div>
+        </div>
+      </header>
+      {/* <!-- ***** Header Area End ***** --> */}
+
+      {/* <!-- ***** Main Banner Area Start ***** --> */}
+      <div className="main-banner" id="top">
+        <div className="container-fluid">
+          <div className="row">
+            <div className="col-lg-6">
+              <div className="left-content">
+                <div className="thumb">
+                  <div className="inner-content">
+                    <h4>We Are Hexashop</h4>
+                    <span>Awesome, clean &amp; creative HTML5 Template</span>
+                    <div className="main-border-button">
+                      <a href="#">Purchase Now!</a>
+                    </div>
+                  </div>
+                  <img src="assets/images/left-banner-image.jpg" alt="" />
+                </div>
+              </div>
+            </div>
+            <div className="col-lg-6">
+              <div className="right-content">
+                <div className="row">
+                  <div className="col-lg-6">
+                    <div className="right-first-image">
+                      <div className="thumb">
+                        <div className="inner-content">
+                          <h4>Women</h4>
+                          <span>Best Clothes For Women</span>
+                        </div>
+                        <div className="hover-content">
+                          <div className="inner">
+                            <h4>Women</h4>
+                            <p>
+                              Lorem ipsum dolor sit amet, conservisii ctetur
+                              adipiscing elit incid.
+                            </p>
+                            <div className="main-border-button">
+                              <a href="#">Discover More</a>
+                            </div>
+                          </div>
+                        </div>
+                        <img src="assets/images/baner-right-image-01.jpg" />
+                      </div>
+                    </div>
+                  </div>
+                  <div className="col-lg-6">
+                    <div className="right-first-image">
+                      <div className="thumb">
+                        <div className="inner-content">
+                          <h4>Men</h4>
+                          <span>Best Clothes For Men</span>
+                        </div>
+                        <div className="hover-content">
+                          <div className="inner">
+                            <h4>Men</h4>
+                            <p>
+                              Lorem ipsum dolor sit amet, conservisii ctetur
+                              adipiscing elit incid.
+                            </p>
+                            <div className="main-border-button">
+                              <a href="#">Discover More</a>
+                            </div>
+                          </div>
+                        </div>
+                        <img src="assets/images/baner-right-image-02.jpg" />
+                      </div>
+                    </div>
+                  </div>
+                  <div className="col-lg-6">
+                    <div className="right-first-image">
+                      <div className="thumb">
+                        <div className="inner-content">
+                          <h4>Kids</h4>
+                          <span>Best Clothes For Kids</span>
+                        </div>
+                        <div className="hover-content">
+                          <div className="inner">
+                            <h4>Kids</h4>
+                            <p>
+                              Lorem ipsum dolor sit amet, conservisii ctetur
+                              adipiscing elit incid.
+                            </p>
+                            <div className="main-border-button">
+                              <a href="#">Discover More</a>
+                            </div>
+                          </div>
+                        </div>
+                        <img src="assets/images/baner-right-image-03.jpg" />
+                      </div>
+                    </div>
+                  </div>
+                  <div className="col-lg-6">
+                    <div className="right-first-image">
+                      <div className="thumb">
+                        <div className="inner-content">
+                          <h4>Accessories</h4>
+                          <span>Best Trend Accessories</span>
+                        </div>
+                        <div className="hover-content">
+                          <div className="inner">
+                            <h4>Accessories</h4>
+                            <p>
+                              Lorem ipsum dolor sit amet, conservisii ctetur
+                              adipiscing elit incid.
+                            </p>
+                            <div className="main-border-button">
+                              <a href="#">Discover More</a>
+                            </div>
+                          </div>
+                        </div>
+                        <img src="assets/images/baner-right-image-04.jpg" />
+                      </div>
+                    </div>
+                  </div>
+                </div>
+              </div>
+            </div>
+          </div>
         </div>
       </div>
-
-      <div className={styles.center}>
-        <Image
-          className={styles.logo}
-          src="/next.svg"
-          alt="Next.js Logo"
-          width={180}
-          height={37}
-          priority
-        />
-      </div>
-
-      <div className={styles.grid}>
-        <a
-          href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className={styles.card}
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2>
-            Docs <span>-&gt;</span>
-          </h2>
-          <p>Find in-depth information about Next.js features and API.</p>
-        </a>
-
-        <a
-          href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className={styles.card}
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2>
-            Learn <span>-&gt;</span>
-          </h2>
-          <p>Learn about Next.js in an interactive course with&nbsp;quizzes!</p>
-        </a>
-
-        <a
-          href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className={styles.card}
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2>
-            Templates <span>-&gt;</span>
-          </h2>
-          <p>Explore the Next.js 13 playground.</p>
-        </a>
-
-        <a
-          href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className={styles.card}
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2>
-            Deploy <span>-&gt;</span>
-          </h2>
-          <p>
-            Instantly deploy your Next.js site to a shareable URL with Vercel.
-          </p>
-        </a>
-      </div>
+      {/* <!-- ***** Main Banner Area End ***** --> */}
     </main>
-  )
+  );
 }
